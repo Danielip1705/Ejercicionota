7ede6a1782dd95b0dcf2ec5ea0b06188be3874c7
diglesias <diglesias@106-16.info2>
2023-09-29 12:55:02 +0200

    ejercicio 2/10/23

--- src/ejercicio1_2/Ejercicio6.java
+++ src/ejercicio1_2/Ejercicio6.java
@@ -6,26 +6,25 @@ public class Ejercicio6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int segundos;
-		final int minutos;
+		int segundoscal;
+		int minutos;
 		int horas;
-		int resultadomin;
-		int resultadohor;
-		Scanner sc = new Scanner(System.in);
+				Scanner sc = new Scanner(System.in);
 
 		System.out.println("Introduzca los segundos que quieras pasar a min y hr");
 
-		segundos = sc.nextInt();
+		segundoscal = sc.nextInt();
 
-		horas = segundos/3600;
+		horas = segundoscal/3600;
 		
-		minutos = segundos/60;
+		minutos = segundoscal%60;
 		
-		segundos = segundos%60;
+		segundoscal = segundoscal/60;
+		
+		segundoscal = segundoscal>60 ? segundoscal-60 : segundoscal;
 		
 		
-		
-		System.out.println( + horas +" horas " + minutos +" minutos " + segundos +" segundos"  );
+		System.out.println( + horas +" horas " + minutos +" minutos " + segundoscal +" segundos"  );
 		
 
 	}
